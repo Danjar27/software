43b50baac74fd6f1db4854091547d273f2ce9bcc
Danjar27 <losteven2018@outlook.com>
2022-06-04 01:00:42 -0500

    set up for jest and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans"],
+      },
+      colors: {
+        blue: {
+          DEFAULT: "#0063A3",
+          light: "#5B96DD",
+          super: "#D6F2FF",
+        },
+        accent: {
+          DEFAULT: "#2F2E41",
+        },
+        red: {
+          DEFAULT: "#E71D36",
+          hover: "#ff5359",
+          light: "#FFF0F2",
+        },
+        gray: {
+          DEFAULT: "#B9B9B9",
+          light: "#D5D5D5",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
